39a6fdae5d0c662413b60e9b5ea9721151428ada
jeaminey <jyoo@strategicgroup.com>
2016-08-01 17:12:59 -0400

    .

---
+++ Week_06_Practical_JS/11_conditionals_this/assignments/01-DOM-DOM-decorator/js/main2.java
@@ -0,0 +1,7 @@
+$(document).ready( function(){
+	console.log("hello");
+	$(".author-name").css({
+		border-style: 'solid',
+		border-color: 'red'
+	});
+});
---
+++ Week_06_Practical_JS/11_conditionals_this/assignments/01-DOM-DOM-decorator/js/main2.js
@@ -0,0 +1,10 @@
+$(document).ready( function(){
+	console.log("hello");
+	$(".author-name").css({
+		border: "1px solid red"
+	});
+	$(".author-related-news-container").css({
+		border: "2px solid orange"
+	});
+	$("img").hide();
+});
